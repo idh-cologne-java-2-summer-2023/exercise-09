c5f8d11ad7d9104f967c287a1d440111037e2d07
WavyLotos <steph@DESKTOP-0JQFQUO.fritz.box>
2023-06-19 19:26:51 +0200

    Aufgabe 2 wenigstens ein Versuch

---
+++ TestTreeListArray.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class TestTreeListArray {
+	List<Integer> Tree = new TreeList<>();
+	List<Integer> Link = new LinkedList<>();
+	List<Integer> Array = new ArrayList<>();
+    //100.000 Zufallzahlen
+	for(int i = 0; i < 100000; i++){
+		Random Hundertausend = new Random();
+		Number.add(i);
+	}
+	//100.000 Zufallzahlen	
+	for(int i = 0; i < 10000; i++) {
+		Random Zehntausend = new Random();
+		Number.add(i);
+	}
+	int max=100000,min=10000;
+    
+    System.out.println(rand.nextInt(max - min) + Tree);
+    System.out.println(rand.nextInt(max - min) + Link);
+    System.out.println(rand.nextInt(max - min) + Array);
+	
+}
+
