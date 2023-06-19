40532c15fbd0d62b0374f0b063fbc3535566f04b
momnii <ischaahmad@172.29.226.40>
2023-06-19 23:25:14 +0200

    Exercise 01 and 02

--- Application.java
+++ Application.java
@@ -6,42 +6,43 @@ import java.util.List;
 public class Application {
 
 	public static void main(String[] args) {
+		//Aufgabe 2
+		
 		//eine Liste mit 100.000 Zufallszahlen erzeugen
 		List<Integer> list = randomList(100000);
-		//eine Liste erzeugen,die zusätzlich 10.000 Zahlen einfügt
+		
+		//eine Liste mit  10.000 zufälligen Zahlen 
+		//ich wusste nicht genau wie ich an zufälligen Stellen 10.000 weitere Zahlen einfügen soll
 		List<Integer> additionalList = randomList(10000);
 	
 						
 		//TreeListe erzeugen und Zeit messen
 		 List<Integer> treeList = new TreeList<>(list);
-		 treeListtime starten = System.currentTimeMillis();
-		 	        
-		 long treeListEndTime = System.currentTimeMillis();
-		 long treeListDuration = treeListEndTime - treeListStartTime;
-		 System.out.println("die TreeList braucht " + treeListDuration + " Millisekunden");
+		 long treeListstarten = System.currentTimeMillis();	        
+		 long treeListzuEnde = System.currentTimeMillis();
+		 long treeListDauer = treeListzuEnde- treeListstarten;
+		 System.out.println("die TreeList braucht " + treeListDauer + " Millisekunden");
 		
 		 //linkedList erzeugen und Zeit messen
 		 List<Integer> linkedList = new LinkedList<>(list);
-	     long linkedListStartTime = System.currentTimeMillis();
-	     insertNumbers(linkedList, additionalList);
-	     long linkedListEndTime = System.currentTimeMillis();
-	     long linkedListDuration = linkedListEndTime - linkedListStartTime;
-	     System.out.println("Die LinkedList braucht " + linkedListDuration + " Millisekunden");
+		 long linkedListstarten = System.currentTimeMillis();
+	     long linkedListzuEnde = System.currentTimeMillis();
+	     long linkedListDauer = linkedListzuEnde - linkedListstarten;
+	     System.out.println("Die LinkedList braucht " + linkedListDauer + " Millisekunden");
 
 	     //ArrayList erzeugen und Zeit messen
 	     List<Integer> arrayList = new ArrayList<>(list);
-	        long arrayListStartTime = System.currentTimeMillis();
-	        insertNumbers(arrayList, additionalList);
-	        long arrayListEndTime = System.currentTimeMillis();
-	        long arrayListDuration = arrayListEndTime - arrayListStartTime;
-	        System.out.println("die ArrayList braucht " + arrayListDuration + " Millisekunden");
+	        long arrayListstarten = System.currentTimeMillis();
+	        long arrayLististzuEnde = System.currentTimeMillis();
+	        long arrayListDauer= arrayLististzuEnde- arrayListstarten;
+	        System.out.println("die ArrayList braucht " + arrayListDauer + " Millisekunden");
 	    }
 
 				
 				
 				}
 		    }
-
+		    //Methode für die LIste mit 100.000 zufälligen Zahlen
 		    private static List<Integer> randomList(int size) {
 		        List<Integer> list = new ArrayList<>(size);
 		        Random random = new Random();
