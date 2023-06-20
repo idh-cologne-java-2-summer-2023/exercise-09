bff5bedecc0a817ba98e6a0e19ceb47ce4d298fe
jhoech2 <92521266+JohannonBlake@users.noreply.github.com>
2023-06-20 16:19:38 +0100

    ugh, finally
    After fighting to get maven working on VSCode I finally gave up and went over to Eclipse. however, my Eclipse and my Git don´t really like each other, so my files might be a bit funky. 
    Also also, I could neither transform the given folder into a maven project (the button just wasn´t there in the right click menu, I double checked with the video) nor could I add a dependency to it because "target project is not a maven project" so, I had to make a new one. Hope this stuff works better in the future

---
+++ exercise09/src/main/java/idh/java/exercise09/TreeList.java
@@ -0,0 +1,7 @@
+package idh.java.exercise09;
+
+public class TreeList {
+	//Alright, finally got Maven setup after 5 hours of fighting VSCode I gave up and
+	//went to Eclipse. So here´s the first commit
+
+}
