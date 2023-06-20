cfb23196de93dc4ec4285b05c04f53418b814879
ImpeccableN <krakatauerich@web.de>
2023-06-20 19:00:19 +0200

    failed

--- source/Tree.java
+++ source/Tree.java
@@ -6,89 +6,81 @@ import java.util.Set;
 
 public class Tree<T>  {
 
-	T value;
-	Set<Tree<T>> children;
+	int value;
+	Set<Tree<Integer>> children;
 	String space = "";
 	
-	public Tree(T value) {
+	public Tree(int value) {
 		this.value = value;
-		this.children = new HashSet<Tree<T>>();
+		this.children = new HashSet<Tree<Integer>>();
 	}
 	
-	public Tree(T value, Collection<Tree<T>> children) {
+	public Tree(int value, Collection<Tree<Integer>> children) {
 		this.value = value;
-		this.children = new HashSet<Tree<T>>(children);
+		this.children = new HashSet<Tree<Integer>>(children);
 	}
 		
-	public T get() {
+	public int get() {
 		return value;
 	}
 
-	public void set(T value) {
+	public void set(int value) {
 		this.value = value;
 	}
 
-	public Set<Tree<T>> children() {
+	public Set<Tree<Integer>> children() {
 		return children;
 	}
 	
 	public void dfs() {
 		System.out.println(space + this.value);
-		for (Tree<T> child: children) {
+		for (Tree<Integer> child: children) {
 			child.space = space + " ";
 			child.dfs();
 		}
 	}
 	
-	public int wsum(int depth) throws Exception{
-		if(value instanceof Integer) {
-			
-			if(depth == 0)
-				return 0;
-			else {
-				for(Tree<Integer> child: children {
-					return (child.value * child.wsum(depth + 1));
-				}
+	public int wsum(int hold, int depth){
+		System.out.println("Depth: " + depth);
+			hold = hold + (depth * value);
+			System.out.println("Hold: " + hold);
+			for(Tree<Integer> child: children) {
+				child.wsum(hold, depth + 1);
 			}
-		return 0;
-		} else {
-			throw new Exception();
-		}
+			return hold;
 	}
 	
 	
 	public static void main(String[] args) {
 		
-		Tree<String> ebike = new Tree<String>("e-bike");
-		Tree<String> tandem = new Tree<String>("tandem");
-		Tree<String> bike = new Tree<String>("bike");
-		Tree<String> buggy = new Tree<String>("buggy");
-		Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
-		Tree<String> babyBuggy = new Tree<String>("babyBuggy");
-
-		wheeled_vehicle.children().add(bike);
-		wheeled_vehicle.children().add(buggy);
-		bike.children().add(tandem);
-		bike.children().add(ebike);
-		buggy.children().add(babyBuggy);
-		
-		wheeled_vehicle.dfs();
+//		Tree<String> ebike = new Tree<String>("e-bike");
+//		Tree<String> tandem = new Tree<String>("tandem");
+//		Tree<String> bike = new Tree<String>("bike");
+//		Tree<String> buggy = new Tree<String>("buggy");
+//		Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
+//		Tree<String> babyBuggy = new Tree<String>("babyBuggy");
+//
+//		wheeled_vehicle.children().add(bike);
+//		wheeled_vehicle.children().add(buggy);
+//		bike.children().add(tandem);
+//		bike.children().add(ebike);
+//		buggy.children().add(babyBuggy);
+//		
+//		wheeled_vehicle.dfs();
 		
 		Tree<Integer> book = new Tree<Integer>(1);
 		Tree<Integer> chapter = new Tree<Integer>(5);
 		Tree<Integer> pages = new Tree<Integer>(210);
 		Tree<Integer> paragraphes = new Tree<Integer>(24);
+		Tree<Integer> words = new Tree<Integer>(51);
 		
 		book.children().add(chapter);
 		chapter.children().add(paragraphes);
 		chapter.children().add(pages);
+		book.children().add(words);
 		
-		try {
-			System.out.println(book.wsum(0));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		book.dfs();
+		System.out.println("Weighted sum: " + book.wsum(0, 0));
 		
 	}
 
